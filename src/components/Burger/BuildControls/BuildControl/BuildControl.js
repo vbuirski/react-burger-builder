c9f74b3113734106c75b139b75529c8be973a726
vbuirski <vonita.buirski@gmail.com>
2020-02-15 11:38:21 +1100

    Adding classNames to BuildControl

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,8 @@ import classes from './BuildControl.module.css';
 const buildControl = (props) => (
  <div className={classes.BuildControl}>
      <div className={classes.Label}>{props.label}</div>
-     <button>Less</button>
-     <button>More</button>
+     <button className={classes.Less}>Less</button>
+     <button className={classes.More}>More</button>
  </div>
 );
 
